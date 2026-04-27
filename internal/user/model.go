aae0ba595ead8f9e28c3cda389c168b2698cb469
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-04-27 13:35:10 +0530

    add user model

---
+++ internal/user/model.go
@@ -0,0 +1,20 @@
+package user
+
+import "time"
+
+type UserType string
+
+const (
+	Admin UserType = "ADMIN"
+	Student UserType = "STUDENT"
+)
+
+type User struct {
+	ID uint `gorm:"primaryKey" json:"id"`
+	Name string `json:"name"`
+	Address string `json:"address"`
+	Mobile string `gorm:"uniqueIndex" json:"mobile"`
+	Role UserType `gorm:"default:'STUDENT'" json:"user_type"`
+	CreatedAt time.Time `gorm:"autoCreatTime" json:"created_at"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+}
