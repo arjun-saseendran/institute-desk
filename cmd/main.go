d70c1255db8fdfc4f2e549a5487fd149e746c12a
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-04-27 14:18:41 +0530

    config main

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,26 @@
 package main
 
+import (
+	"log"
+
+	"github.com/arjun-saseendran/institute/internal/db"
+	"github.com/arjun-saseendran/institute/internal/user"
+	"github.com/gin-gonic/gin"
+)
+
 
 func main(){
+	dbConnection, err := db.ConnectDB()
+	if err != nil {
+		log.Fatalf("failed to connect database: %v",err)
+	}
+	
+	router := gin.Default()
+	
+	userService := user.NewUserService(dbConnection)
+	userHandler := user.NewUserHandler(userService)
+	userHandler.RegisterEndPoints(router)
+	
+	router.Run(":3000")
 	
 }
