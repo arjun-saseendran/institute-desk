07bf1f2ae6074ff56b9a02640d72a1d731ff4973
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-04-27 13:54:19 +0530

    add user service

--- internal/user/service.go
+++ internal/user/service.go
@@ -1,2 +1,22 @@
 package user
 
+import "gorm.io/gorm"
+
+type UserService interface {
+	Create(userData *InputUser) (*User, error)
+}
+
+type userService struct{ db *gorm.DB }
+
+func NewUserService(db *gorm.DB) UserService {
+	return &userService{db: db}
+}
+
+func (us *userService) Create(userData *InputUser) (*User, error) {
+	newUser := &User{Name: userData.Name, Address: userData.Address, Mobile: userData.Mobile}
+	result := us.db.Create(newUser)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return newUser, nil
+}
