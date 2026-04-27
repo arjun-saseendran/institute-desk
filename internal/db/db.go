14ff6aad009b5a31e166d14833b6ddf4cfc74ed7
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-04-27 14:14:18 +0530

    config postgres

---
+++ internal/db/db.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"log"
+	"os/user"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() (*gorm.DB, error) {
+
+	dsn := "host=localhost user=postgres password=postgres dbname=swimming-pool port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN:                  dsn,
+		PreferSimpleProtocol: true,
+	}), &gorm.Config{})
+
+	if err != nil {
+
+		return nil, err
+	}
+
+	log.Println("Successfully connected to Postgres!")
+
+	err = db.AutoMigrate(&user.User{})
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Database migration completed successfully!")
+
+	return db, nil
+}
